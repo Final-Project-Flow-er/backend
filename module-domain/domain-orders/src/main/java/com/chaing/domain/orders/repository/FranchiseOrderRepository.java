64659389afcc63002c33308c162a04d52c3fdfce
chaewookim <chaewookim2877@gmail.com>
2026-02-15 18:41:14 +0900

    [feat] #6 FranchiseOrderRepository 구현

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/repository/FranchiseOrderRepository.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/repository/FranchiseOrderRepository.java
@@ -1,4 +1,12 @@
 package com.chaing.domain.orders.repository;
 
-public interface FranchiseOrderRepository {
+import com.chaing.domain.orders.entity.FranchiseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FranchiseOrderRepository extends JpaRepository<FranchiseOrder, Long> {
+    List<FranchiseOrder> findAllByFranchiseIdAndUsername(Long franchiseId, String username);
 }

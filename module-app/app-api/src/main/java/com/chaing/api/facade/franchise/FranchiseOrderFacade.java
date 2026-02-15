597807bc5eb5bc8dc832949a4c655a97bdee1ddc
chaewookim <chaewookim2877@gmail.com>
2026-02-15 18:39:59 +0900

    [feat] #6 FranchiseOrderFacade 구현

--- module-app/app-api/src/main/java/com/chaing/api/facade/franchise/FranchiseOrderFacade.java
+++ module-app/app-api/src/main/java/com/chaing/api/facade/franchise/FranchiseOrderFacade.java
@@ -1,4 +1,29 @@
 package com.chaing.api.facade.franchise;
 
+import com.chaing.api.dto.franchise.orders.response.FranchiseOrderResponse;
+import com.chaing.domain.orders.entity.FranchiseOrder;
+import com.chaing.domain.orders.service.FranchiseOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FranchiseOrderFacade {
+
+    private final FranchiseOrderService franchiseOrderService;
+//    private final FranchiseService franchiseService;
+
+    // 가맹점 발주 조회
+    public List<FranchiseOrderResponse> getAllOrders(String username) {
+        // franchiseId username으로 조회하는 로직 추가 필요
+        Long franchiseId = 1L;
+
+        List<FranchiseOrder> orders = franchiseOrderService.getAllOrders(franchiseId, username);
+
+        return FranchiseOrderResponse.from(orders);
+    }
 }

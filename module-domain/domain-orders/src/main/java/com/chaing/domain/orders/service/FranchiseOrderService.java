32828c417278c8abaf4feb022f3e2c6042ae4ebc
chaewookim <chaewookim2877@gmail.com>
2026-02-15 19:13:20 +0900

    [feat] #6 FranchiseOrderService 구현
    가맹점 특정 발주 조회 구현

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderService.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderService.java
@@ -17,4 +17,10 @@ public class FranchiseOrderService {
     public List<FranchiseOrder> getAllOrders(Long franchiseId, String username) {
         return franchiseOrderRepository.findAllByFranchiseIdAndUsername(franchiseId, username);
     }
+
+    // 발주 번호에 따른 가맹점 특정 발주 조회
+    public FranchiseOrder getOrder(Long franchiseId, String username, String orderCode) {
+        return franchiseOrderRepository.findByFranchiseIdAndUsernameAndOrderCode(franchiseId, username, orderCode)
+                .orElseThrow(() -> new FranchiseOrderException(FranchiseOrderErrorCode.ORDER_NOT_FOUND));
+    }
 }

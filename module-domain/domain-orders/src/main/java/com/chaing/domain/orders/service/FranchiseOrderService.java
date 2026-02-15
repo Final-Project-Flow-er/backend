43803e33214bf4f10a092895043554015e25b022
chaewookim <chaewookim2877@gmail.com>
2026-02-15 18:46:23 +0900

    [feat] #6 FranchiseOrderService 구현
    가맹점 발주 목록 조회 로직 구현

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderService.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderService.java
@@ -1,4 +1,20 @@
 package com.chaing.domain.orders.service;
 
+import com.chaing.domain.orders.entity.FranchiseOrder;
+import com.chaing.domain.orders.repository.FranchiseOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class FranchiseOrderService {
+
+    private final FranchiseOrderRepository franchiseOrderRepository;
+
+    // 가맹점 발주 목록 조회
+    public List<FranchiseOrder> getAllOrders(Long franchiseId, String username) {
+        return franchiseOrderRepository.findAllByFranchiseIdAndUsername(franchiseId, username);
+    }
 }

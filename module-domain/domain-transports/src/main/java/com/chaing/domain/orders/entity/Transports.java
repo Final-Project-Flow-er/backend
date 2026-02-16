c5dd6471a53c2099466141d99bb358677c31ae28
yoocy0 <yoocy0328@gmail.com>
2026-02-16 14:30:25 +0900

    [feat] #23 엔티티 운송 엔í업체 엔티티 구현

--- module-domain/domain-transports/src/main/java/com/chaing/domain/orders/entity/Transports.java
+++ module-domain/domain-transports/src/main/java/com/chaing/domain/orders/entity/Transports.java
@@ -19,33 +19,33 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Transport extends BaseEntity {
+public class Transports extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long transportId;
+    private Long transportId;
 
     @Column(nullable = false)
-    String companyName;
+    private String companyName;
 
     @Column(nullable = false)
-    String manager;                 // 운송 업체 담당자(대표)명
+    private String manager;                 // 운송 업체 담당자(대표)명
 
     @Column(nullable = false)
-    String office_phone;            // 업체 전화번호
+    private String office_phone;            // 업체 전화번호
 
     @Column(nullable = false)
-    String address;
+    private String address;
 
     @Column(nullable = false)
-    Integer owned_vehicles;         // 보유 차량 대수
+    private Integer owned_vehicles;         // 보유 차량 대수
 
     @Column(nullable = false)
-    Integer unit_price;             // 운송 단가(박스 + km 당)
+    private Integer unit_price;             // 운송 단가(박스 + km 당)
 
     @Column(nullable = false)
-    LocalDate contract_start_date;  // 계약 시작일
+    private LocalDate contract_start_date;  // 계약 시작일
 
     @Column(nullable = false)
-    LocalDate contract_end_date;    // 계약 종료일
+    private LocalDate contract_end_date;    // 계약 종료일
 }

3d81232180d3fc9acb8e6473359134a82c900eb6
yk5095 <kyk5095@gmail.com>
2026-02-17 18:42:19 +0900

    [feat] #15 settlement VoucherTyp enums 구현

--- module-domain/domain-settlements/src/main/java/com/chaing/domain/settlements/enums/SettlementStatus.java
+++ module-domain/domain-settlements/src/main/java/com/chaing/domain/settlements/enums/SettlementStatus.java
@@ -1,7 +1,7 @@
 package com.chaing.domain.settlements.enums;
 
 public enum SettlementStatus {
-    DRAFT, //생성
+    DRAFT, // 생성
     CALCULATED, // 계산 완료
     CONFIREMD, // 본사 확정 완료
     CANCELED // 취소
---
+++ module-domain/domain-settlements/src/main/java/com/chaing/domain/settlements/enums/VoucherType.java
@@ -0,0 +1,11 @@
+package com.chaing.domain.settlements.enums;
+
+public enum VoucherType {
+    SALES, // 매출
+    ORDER, // 발주대금
+    DELIVERY, // 배송비
+    COMMISSION, // 수수료
+    LOSS, // 손실
+    REFUND, // 반품환급
+    ADJUSTMENT // 조정
+}

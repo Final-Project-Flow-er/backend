266b9398e56ee91fc4757ff44cd7781688279352
chaewookim <chaewookim2877@gmail.com>
2026-02-18 13:40:04 +0900

    [feat] #6 발주 생성 시 예외 코드 작성

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/exception/FranchiseOrderErrorCode.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/exception/FranchiseOrderErrorCode.java
@@ -11,6 +11,8 @@ public enum FranchiseOrderErrorCode implements ErrorCode {
     // 404 NOT FOUND
     ORDER_NOT_FOUND(404, "FO001", "해당 발주를 찾을 수 없습니다."),
 
+    PRODUCT_NOT_FOUND(404, "PR001", "해당 제품을 찾을 수 없습니다."),
+
     // 400 BAD REQUEST
     ORDER_INVALID_STATUS(400, "FO002" ,"발주 상태가 대기일 때만 수정 가능합니다.");
 

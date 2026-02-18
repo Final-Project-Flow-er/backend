6999ce5e5ad8138c666739fec44a7196e67636ae
rlatjddms <kseo_o@naver.com>
2026-02-18 22:02:30 +0900

    [feat] #29 headquarter 도메인 entity 구현

---
+++ module-domain/domain-headquarter/src/main/java/com/chaing/domain/headquarter/entity/Headquarter.java
@@ -0,0 +1,40 @@
+package com.chaing.domain.headquarter.entity;
+
+import com.chaing.core.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Headquarter extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hqId;
+
+    @Column(nullable = false, unique = true)
+    private String hqCode;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String representativeName;
+}

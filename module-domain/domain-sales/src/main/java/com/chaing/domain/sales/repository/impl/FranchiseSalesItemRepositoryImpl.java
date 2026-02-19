517a17bfc7a602498abe3c717d74d77413f9b8a7
chaewookim <chaewookim2877@gmail.com>
2026-02-19 15:46:54 +0900

    [feat] #7 QueryDSL을 위한 인터페이스 및 클래스 구현

--- module-domain/domain-sales/src/main/java/com/chaing/domain/sales/repository/impl/FranchiseSalesItemRepositoryImpl.java
+++ module-domain/domain-sales/src/main/java/com/chaing/domain/sales/repository/impl/FranchiseSalesItemRepositoryImpl.java
@@ -1,4 +1,40 @@
 package com.chaing.domain.sales.repository.impl;
 
-public class FranchiseSalesItemRepositoryImpl {
+import com.chaing.domain.sales.dto.response.FranchiseSalesInfoResponse;
+import com.chaing.domain.sales.dto.response.QFranchiseSalesInfoResponse;
+import com.chaing.domain.sales.repository.interfaces.FranchiseSalesItemRepositoryCustom;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.chaing.domain.sales.entity.QSales.sales;
+import static com.chaing.domain.sales.entity.QSalesItem.salesItem;
+
+@Repository
+@RequiredArgsConstructor
+public class FranchiseSalesItemRepositoryImpl implements FranchiseSalesItemRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<FranchiseSalesInfoResponse> searchAllSalesItems(Long franchiseId) {
+        return queryFactory
+                .select(new QFranchiseSalesInfoResponse(
+                        sales.salesCode,
+                        salesItem.createdAt,
+                        salesItem.productCode,
+                        salesItem.productName,
+                        salesItem.quantity,
+                        salesItem.unitPrice,
+                        sales.totalAmount
+                ))
+                .from(salesItem)
+                .join(salesItem.sales, sales)
+                .where(
+                        sales.franchiseId.eq(franchiseId)
+                )
+                .fetch();
+    }
 }
--- module-domain/domain-sales/src/main/java/com/chaing/domain/sales/repository/interfaces/FranchiseSalesItemRepositoryCustom.java
+++ module-domain/domain-sales/src/main/java/com/chaing/domain/sales/repository/interfaces/FranchiseSalesItemRepositoryCustom.java
@@ -1,4 +1,10 @@
-package com.chaing.domain.sales.repository;
+package com.chaing.domain.sales.repository.interfaces;
 
-public interface FranchiseSalesRepositoryCustom {
+import com.chaing.domain.sales.dto.response.FranchiseSalesInfoResponse;
+
+import java.util.List;
+
+public interface FranchiseSalesItemRepositoryCustom {
+    // 판매 목록 조회
+    List<FranchiseSalesInfoResponse> searchAllSalesItems(Long franchiseId);
 }

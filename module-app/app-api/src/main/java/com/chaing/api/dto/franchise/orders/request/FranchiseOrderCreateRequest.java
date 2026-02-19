f5cb0b9462652cafe2a98d7ef090815d83a1b337
chaewookim <60053929+chaewoo-kim@users.noreply.github.com>
2026-02-19 09:27:23 +0900

    Feat/flow 13 order franchise (#25)
    - [Create] 가맹점 발주 생성 API 구현
    - [Update] 가맹점 발주 수정 및 취소 API 구현
    - [Read] 가맹점 발주 조회 API 구현
    - [Chore] 멀티 모듈 기반 구축

--- module-app/app-api/src/main/java/com/chaing/api/dto/franchise/orders/request/FranchiseOrderCreateRequest.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/franchise/orders/request/FranchiseOrderCreateRequest.java
@@ -1,4 +1,49 @@
 package com.chaing.api.dto.franchise.orders.request;
 
-public record FranchiseOrderCreateRequest() {
+import com.chaing.domain.orders.dto.command.FranchiseOrderCreateCommand;
+import com.chaing.domain.orders.dto.info.FranchiseOrderCreateInfo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record FranchiseOrderCreateRequest(
+        @NotBlank
+        String username,
+
+        @NotBlank
+        String phoneNumber,
+
+        @NotNull
+        LocalDateTime deliveryDate,
+
+        @NotBlank
+        String address,
+
+        String requirement,
+
+        List<FranchiseOrderCreateRequestItem> items
+) {
+    public FranchiseOrderCreateCommand toFranchiseOrderCreateCommand() {
+        return FranchiseOrderCreateCommand.builder()
+                .username(this.username)
+                .phoneNumber(this.phoneNumber)
+                .deliveryDate(this.deliveryDate)
+                .address(this.address)
+                .requirement(this.requirement)
+                .items(FranchiseOrderCreateRequest.from(this.items))
+                .build();
+    }
+
+    private static List<FranchiseOrderCreateInfo> from(List<FranchiseOrderCreateRequestItem> items) {
+        return items.stream()
+                .map(item -> {
+                    return new FranchiseOrderCreateInfo(
+                            item.productCode(),
+                            item.quantity()
+                    );
+                })
+                .toList();
+    }
 }

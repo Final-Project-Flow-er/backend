ca85227f2800e4c5526afc8a7ee6f47e2041b659
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-23 16:19:23 +0900

    [feat] #36 제약조건 추가

--- module-domain/domain-inventories/src/main/java/com/chaing/domain/inventories/entity/FactoryInventoryPolicy.java
+++ module-domain/domain-inventories/src/main/java/com/chaing/domain/inventories/entity/FactoryInventoryPolicy.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -18,6 +20,9 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+        uniqueConstraints = @UniqueConstraint(columnNames = {"franchiseId", "productId"})
+)
 public class FactoryInventoryPolicy extends BaseEntity {
 
     @Id

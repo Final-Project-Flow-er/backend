6ef85dcceae554022bdc8151b3c16537fa391eb3
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-24 16:27:36 +0900

    [feat] #54 FranchiseInventoryLog Controller 추가

---
+++ module-app/app-api/src/main/java/com/chaing/api/controller/hq/HQInventoryLogController.java
@@ -0,0 +1,75 @@
+package com.chaing.api.controller.hq;
+
+import com.chaing.api.dto.hq.inventorylogs.request.HQFranchiseLogRequest;
+import com.chaing.api.dto.hq.inventorylogs.request.HQLogRequest;
+import com.chaing.core.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
+
+@RestController
+@Tag(name = "HqInventoryLog API", description = "본사 재고 로그 관련 API")
+@RequestMapping("/api/v1/hq/log")
+
+public class HQInventoryLogController {
+
+    @Operation(summary = "본사 반품 입고 이력 조회", description = "본사의 반품 입고 이력을 확인합니다.")
+    @GetMapping("/return-inbound")
+    public ResponseEntity<ApiResponse<Page<?>>> findReturnInboundLogs(
+            @RequestBody HQLogRequest hqLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @Operation(summary = "본사 반품 출고 이력 조회", description = "본사의 반품 출고 이력을 확인합니다.")
+    @GetMapping("/return-outbound/{franchiseId}")
+    public ResponseEntity<ApiResponse<Page<?>>> findReturnOutboundLogs(
+            @PathVariable Long franchiseId,
+            @RequestBody HQLogRequest hqLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @Operation(summary = "본사 폐기 이력 조회", description = "본사의 폐기 이력을 확인합니다.")
+    @GetMapping("/disposal")
+    public ResponseEntity<ApiResponse<Page<?>>> findDisposalLogs(
+            @RequestBody HQLogRequest hqLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @Operation(summary = "가맹점 물류 입출고 이력 조회", description = "본사에서 가맹점 물류 입출고 이력을 확인합니다.")
+    @GetMapping("/franchise-inventory/{franchiseId}")
+    public ResponseEntity<ApiResponse<Page<?>>> findFranchiseInboundOutboundLogs(
+            @PathVariable Long franchiseId,
+            @RequestBody HQFranchiseLogRequest hqFranchiseLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @Operation(summary = "가맹점 판매 환불 이력 조회", description = "본사에서 가맹점 판매 환불 이력을 확인합니다.")
+    @GetMapping("/franchise-sales/{franchiseId}")
+    public ResponseEntity<ApiResponse<Page<?>>> findFranchiseSalesRefundLogs(
+            @PathVariable Long franchiseId,
+            @RequestBody HQFranchiseLogRequest hqFranchiseLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @Operation(summary = "공장 재고 이력 조회", description = "본사에서 공장의 재고 이력을 확인합니다.")
+    @GetMapping("/factory/{factoryId}")
+    public ResponseEntity<ApiResponse<Page<?>>> findFactoryInventoryLogs(
+            @PathVariable Long factoryId,
+            @RequestBody HQLogRequest hqLogRequest,
+            Pageable pageable) {
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+}

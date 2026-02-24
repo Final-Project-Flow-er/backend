464f9fcd6d00779595db09f4f554dba2def12301
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-24 12:25:27 +0900

    [feat] #42 검증 추가

--- module-app/app-api/src/main/java/com/chaing/api/dto/hq/inventories/request/HQFranchiseItemsRequest.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/hq/inventories/request/HQFranchiseItemsRequest.java
@@ -1,10 +1,14 @@
 package com.chaing.api.dto.hq.inventories.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record HQFranchiseItemsRequest(
+        @NotNull
         Long franchiseId,
+        @NotNull
         Long productId,
-        LocalDate productionDate
+        LocalDate manufactureDate
 ) {
 }

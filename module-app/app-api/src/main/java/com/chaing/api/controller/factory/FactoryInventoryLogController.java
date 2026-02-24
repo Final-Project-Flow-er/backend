d89feb91846101025635ff43865b1e9021cda49e
cho-yunho01 <04whdbsgh30@naver.com>
2026-02-24 17:17:09 +0900

    [fix] #54 pageable 제거

--- module-app/app-api/src/main/java/com/chaing/api/controller/factory/FactoryInventoryLogController.java
+++ module-app/app-api/src/main/java/com/chaing/api/controller/factory/FactoryInventoryLogController.java
@@ -4,15 +4,15 @@ import com.chaing.api.dto.factory.inventorylogs.request.FactoryLogRequest;
 import com.chaing.core.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
 
 @RestController
 @Tag(name = "FactoryInventoryLog API", description = "공장 재고 로그 관련 API")
@@ -20,11 +20,11 @@ import java.awt.print.Pageable;
 
 public class FactoryInventoryLogController {
     @Operation(summary = "공장 재고 이력 조회", description = "공장의 재고 이력을 확인합니다.")
-    @GetMapping("/{factoryId}")
-    public ResponseEntity<ApiResponse<Page<?>>> findFactoryInventoryLogs(
+    @PostMapping("/{factoryId}")
+    public ResponseEntity<ApiResponse<?>> findFactoryInventoryLogs(
             @PathVariable Long factoryId,
-            @RequestBody FactoryLogRequest factoryLogRequest,
-            Pageable pageable) {
+            @RequestBody FactoryLogRequest factoryLogRequest
+    ) {
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
--- module-app/app-api/src/main/java/com/chaing/api/controller/franchise/FranchiseInventoryLogController.java
+++ module-app/app-api/src/main/java/com/chaing/api/controller/franchise/FranchiseInventoryLogController.java
@@ -4,15 +4,13 @@ import com.chaing.api.dto.franchise.inventorylogs.request.FranchiseLogRequest;
 import com.chaing.core.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.print.Pageable;
 
 @RestController
 @Tag(name = "FranchiseInventoryLog API", description = "가맹점 재고 로그 관련 API")
@@ -21,20 +19,20 @@ import java.awt.print.Pageable;
 public class FranchiseInventoryLogController {
 
     @Operation(summary = "가맹점 물류 입출고 이력 조회", description = "가맹점 물류 입출고 이력을 확인합니다.")
-    @GetMapping("/inventory/{franchiseId}")
-    public ResponseEntity<ApiResponse<Page<?>>> findFranchiseInboundOutboundLogs(
+    @PostMapping("/inventory/{franchiseId}")
+    public ResponseEntity<ApiResponse<?>> findFranchiseInboundOutboundLogs(
             @PathVariable Long franchiseId,
-            @RequestBody FranchiseLogRequest FranchiseLogRequest,
-            Pageable pageable) {
+            @RequestBody FranchiseLogRequest FranchiseLogRequest
+            ) {
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 
     @Operation(summary = "가맹점 판매 환불 이력 조회", description = "가맹점 판매 환불 이력을 확인합니다.")
-    @GetMapping("/sales/{franchiseId}")
-    public ResponseEntity<ApiResponse<Page<?>>> findFranchiseSalesRefundLogs(
+    @PostMapping("/sales/{franchiseId}")
+    public ResponseEntity<ApiResponse<?>> findFranchiseSalesRefundLogs(
             @PathVariable Long franchiseId,
-            @RequestBody FranchiseLogRequest FranchiseLogRequest,
-            Pageable pageable) {
+            @RequestBody FranchiseLogRequest FranchiseLogRequest
+            ) {
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 

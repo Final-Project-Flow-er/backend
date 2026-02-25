20d5e5d58bb37af411b69a2abbc8de0db8e3e877
chaewookim <chaewookim2877@gmail.com>
2026-02-25 14:39:24 +0900

    [feat] #7 반품 생성 DTO 구현

--- module-app/app-api/src/main/java/com/chaing/api/dto/franchise/returns/request/FranchiseReturnCreateRequest.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/franchise/returns/request/FranchiseReturnCreateRequest.java
@@ -1,4 +1,21 @@
 package com.chaing.api.dto.franchise.returns.request;
 
-public record FranchiseReturnCreateRequest() {
+import com.chaing.domain.returns.dto.request.FranchiseReturnItemCreateRequest;
+import com.chaing.domain.returns.enums.ReturnType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record FranchiseReturnCreateRequest(
+        @NotBlank
+        String orderCode,
+
+        @NotNull
+        ReturnType returnType,
+
+        String description,
+
+        List<FranchiseReturnItemCreateRequest> items
+) {
 }
--- module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/request/FranchiseReturnItemCreateRequest.java
+++ module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/request/FranchiseReturnItemCreateRequest.java
@@ -1,4 +1,21 @@
 package com.chaing.domain.returns.dto.request;
 
-public record FranchiseReturnItemCreateRequest() {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record FranchiseReturnItemCreateRequest(
+        @NotBlank
+        String boxCode,
+
+        @NotBlank
+        String productCode,
+
+        @NotBlank
+        String productName,
+
+        @NotNull
+        @Min(1)
+        Integer quantity
+) {
 }

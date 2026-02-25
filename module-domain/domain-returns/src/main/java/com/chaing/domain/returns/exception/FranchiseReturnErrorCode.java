44ead0f4a2622fda7a0a150955b305dae9e8da92
chaewookim <chaewookim2877@gmail.com>
2026-02-25 14:24:55 +0900

    [feat] #7 반품 취소 예외 코드 작성

--- module-domain/domain-returns/src/main/java/com/chaing/domain/returns/exception/FranchiseReturnErrorCode.java
+++ module-domain/domain-returns/src/main/java/com/chaing/domain/returns/exception/FranchiseReturnErrorCode.java
@@ -10,7 +10,10 @@ public enum FranchiseReturnErrorCode implements ErrorCode {
 
     // 404 NOT FOUND
     RETURN_NOT_FOUND(404, "RE001", "해당 반품을 찾을 수 없습니다."),
-    PRODUCT_NOT_FOUND(404, "RE002", "해당 제품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND(404, "RE002", "해당 제품을 찾을 수 없습니다."),
+
+    // 409 CONFLICT
+    CANCEL_NOT_ALLOWED(409, "RE003", "반품의 상태가 대기일 때만 취소 가능합니다.");
 
     // 400 BAD REQUEST
 

56948f2f5512304b5957c78539e56808504c1df3
chaewookim <chaewookim2877@gmail.com>
2026-02-26 17:18:06 +0900

    [feat] #66 본사 발주 조회 Service 로직 구현

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/HQOrderService.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/HQOrderService.java
@@ -1,24 +1,55 @@
 package com.chaing.domain.orders.service;
 
-import com.chaing.domain.orders.dto.response.HQOrderResponse;
+import com.chaing.domain.orders.dto.info.HQOrderInfo;
 import com.chaing.domain.orders.entity.HeadOfficeOrder;
 import com.chaing.domain.orders.entity.HeadOfficeOrderItem;
+import com.chaing.domain.orders.exception.HQOrderErrorCode;
+import com.chaing.domain.orders.exception.HQOrderException;
+import com.chaing.domain.orders.repository.HeadOfficeOrderItemRepository;
 import com.chaing.domain.orders.repository.HeadOfficeOrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class HQOrderService {
 
     private final HeadOfficeOrderRepository orderRepository;
+    private final HeadOfficeOrderItemRepository orderItemRepository;
 
-    public List<HQOrderResponse> getAllOrders(Long hqId, String username) {
+    // 발주 정보 조회
+    // hqId, username으로 order 조회
+    public Map<Long, HQOrderInfo> getAllOrders(Long hqId, String username) {
         List<HeadOfficeOrder> orders = orderRepository.findAllByHqIdAndUsername(hqId, username);
-        Map<HeadOfficeOrder, List<HeadOfficeOrderItem>> orderAndOrderItem = new HashMap<>();
+
+        return orders.stream()
+                .collect(Collectors.toMap(
+                        HeadOfficeOrder::getHeadOfficeOrderId,
+                        HQOrderInfo::from
+                ));
+    }
+
+    // 발주 제품 정보 조회
+    // orderId로 orderItem 조회
+    // Map<orderId, List<productId>>
+    public Map<Long, List<Long>> getAllOrderItems(Long hqId, List<Long> orderIds) {
+        List<HeadOfficeOrderItem> orderItems = orderItemRepository.findAllByHeadOfficeOrder_HqIdAndHeadOfficeOrder_HeadOfficeOrderIdIn(hqId, orderIds);
+
+        if (orderItems == null || orderItems.isEmpty()) {
+            throw new HQOrderException(HQOrderErrorCode.ORDER_NOT_FOUND);
+        }
+
+        return orderItems.stream()
+                .collect(Collectors.groupingBy(
+                        item -> item.getHeadOfficeOrder().getHeadOfficeOrderId(),
+                        Collectors.mapping(
+                                HeadOfficeOrderItem::getProductId,
+                                Collectors.toList()
+                        )
+                ));
     }
 }

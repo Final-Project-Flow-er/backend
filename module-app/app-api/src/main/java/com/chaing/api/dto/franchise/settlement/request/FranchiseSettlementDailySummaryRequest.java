1814c11506040fb7b87ef51bf18de6946999c3e1
yk5095 <kyk5095@gmail.com>
2026-02-26 16:45:25 +0900

    [feat] #50 가맹점 정산요약 request 구현

---
+++ module-app/app-api/src/main/java/com/chaing/api/dto/franchise/settlement/request/FranchiseSettlementDailySummaryRequest.java
@@ -0,0 +1,14 @@
+package com.chaing.api.dto.franchise.settlement.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class FranchiseSettlementDailySummaryRequest {
+    @Schema(description = "조회 날짜", example = "2026-02-26", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate date;
+}

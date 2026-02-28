04d91addeae19dc3af2b119a64980b55a13510e3
rlatjddms <kseo_o@naver.com>
2026-02-28 18:15:37 +0900

    [feat] #75 CustomUserDetailsService 및 UserPrincipal 추가

---
+++ module-app/app-api/src/main/java/com/chaing/api/security/principal/UserPrincipal.java
@@ -0,0 +1,45 @@
+package com.chaing.api.security.principal;
+
+import com.chaing.domain.users.entity.User;
+import com.chaing.domain.users.enums.UserRole;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+
+    private final Long id;
+    private final String loginId;
+    private final String password;
+    private final UserRole role;
+
+    public UserPrincipal(User user) {
+        this.id = user.getUserId();
+        this.loginId = user.getLoginId();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() { return password; }
+    @Override
+    public String getUsername() { return loginId; }
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
+}

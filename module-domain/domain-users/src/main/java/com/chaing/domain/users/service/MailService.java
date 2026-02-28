a05536a59512dda47f6645edbc4ea62e08472916
rlatjddms <kseo_o@naver.com>
2026-02-28 18:14:06 +0900

    [feat] #75 MailService 인터페이스 구현

---
+++ module-domain/domain-users/src/main/java/com/chaing/domain/users/service/MailService.java
@@ -0,0 +1,7 @@
+package com.chaing.domain.users.service;
+
+public interface MailService {
+
+    void sendRegisterMail(String email, String loginId, String tempPassword, String employeeNumber);
+    void sendTempPassword(String email, String tempPassword);
+}

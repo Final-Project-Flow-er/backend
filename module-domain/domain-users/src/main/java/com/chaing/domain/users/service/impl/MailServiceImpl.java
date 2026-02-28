11cd2b35d95123e83e7fdfabe3a7a8456b90bffa
rlatjddms <kseo_o@naver.com>
2026-02-28 23:52:05 +0900

    [chore] #75 MailServiceImpl 사용하지 않는 import 제거

--- module-domain/domain-users/src/main/java/com/chaing/domain/users/service/impl/MailServiceImpl.java
+++ module-domain/domain-users/src/main/java/com/chaing/domain/users/service/impl/MailServiceImpl.java
@@ -6,7 +6,6 @@ import com.chaing.domain.users.service.MailService;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;

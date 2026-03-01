3b362d9b07801362da6b0f500ef4874dbf72d538
chaewookim <chaewookim2877@gmail.com>
2026-03-01 16:31:47 +0900

    [feat] #66 본사 발주 예외 처리 추가

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/exception/HQOrderErrorCode.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/exception/HQOrderErrorCode.java
@@ -10,7 +10,10 @@ public enum HQOrderErrorCode implements ErrorCode {
 
     // 404 NOT FOUND
     ORDER_NOT_FOUND(404, "HO001", "해당 발주를 찾을 수 없습니다."),
-    ORDER_ITEM_NOT_FOUND(404, "HO002", "해당 발주의 제품을 찾을 수 없습니다.");
+    ORDER_ITEM_NOT_FOUND(404, "HO002", "해당 발주의 제품을 찾을 수 없습니다."),
+
+    // 400 BAD REQUEST
+    INVALID_INPUT(400, "HO003", "잘못된 입력값입니다.");
 
     private final Integer status;
     private final String code;

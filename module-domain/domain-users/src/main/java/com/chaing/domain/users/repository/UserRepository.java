836bbcb94984e70c1eeb79de256c7bc009129354
rlatjddms <kseo_o@naver.com>
2026-03-01 14:42:59 +0900

    [refactor] #75 사원번호 및 로그인 아이디 생성 시 동시성 문제 해결

--- module-domain/domain-users/src/main/java/com/chaing/domain/users/repository/UserRepository.java
+++ module-domain/domain-users/src/main/java/com/chaing/domain/users/repository/UserRepository.java
@@ -1,24 +1,28 @@
-package com.chaing.domain.users.repository;
+    package com.chaing.domain.users.repository;
 
-import com.chaing.domain.users.entity.User;
-import com.chaing.domain.users.enums.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+    import com.chaing.domain.users.entity.User;
+    import com.chaing.domain.users.enums.UserRole;
+    import jakarta.persistence.LockModeType;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Lock;
+    import org.springframework.data.jpa.repository.Query;
+    import org.springframework.data.repository.query.Param;
+    import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+    @Repository
+    public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByLoginId(String loginId);
+        Optional<User> findByLoginId(String loginId);
 
-    @Query("SELECT MAX(u.employeeNumber) FROM User u WHERE u.role = :role")
-    Optional<String> findMaxEmployeeNumberByRole(@Param("role") UserRole role);
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        @Query("SELECT MAX(u.employeeNumber) FROM User u WHERE u.role = :role")
+        Optional<String> findMaxEmployeeNumberByRole(@Param("role") UserRole role);
 
-    @Query("SELECT MAX(u.loginId) FROM User u WHERE u.role = :role")
-    Optional<String> findMaxLoginIdByRole(@Param("role") UserRole role);
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        @Query("SELECT MAX(u.loginId) FROM User u WHERE u.role = :role")
+        Optional<String> findMaxLoginIdByRole(@Param("role") UserRole role);
 
-    boolean existsByEmail(String email);
-}
+        boolean existsByEmail(String email);
+    }

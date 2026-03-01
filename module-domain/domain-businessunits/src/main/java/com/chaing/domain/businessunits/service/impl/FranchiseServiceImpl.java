11c4ef2a8ddf393bd1c29ea6648763238e767a3d
rlatjddms <kseo_o@naver.com>
2026-03-02 00:27:59 +0900

    [feat] #78 FranchiseServiceImpl 구현

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/impl/FranchiseServiceImpl.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/impl/FranchiseServiceImpl.java
@@ -1,11 +1,17 @@
 package com.chaing.domain.businessunits.service.impl;
 
 import com.chaing.core.enums.UsableStatus;
+import com.chaing.domain.businessunits.dto.command.BusinessUnitCreateCommand;
 import com.chaing.domain.businessunits.dto.command.BusinessUnitUpdateCommand;
 import com.chaing.domain.businessunits.dto.internal.BusinessUnitInternal;
+import com.chaing.domain.businessunits.entity.Franchise;
+import com.chaing.domain.businessunits.exception.BusinessUnitErrorCode;
+import com.chaing.domain.businessunits.exception.BusinessUnitException;
 import com.chaing.domain.businessunits.repository.FranchiseRepository;
 import com.chaing.domain.businessunits.service.BusinessUnitManagementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,28 +20,48 @@ public class FranchiseServiceImpl implements BusinessUnitManagementService {
 
     private final FranchiseRepository franchiseRepository;
 
+    // 가맹점 아이디로 가맹점 조회
     @Override
     public BusinessUnitInternal getById(Long id) {
-        return null;
+        return franchiseRepository.findById(id)
+                .map(BusinessUnitInternal::from)
+                .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
     }
 
+    // 가맹점 정보 수정
     @Override
     public void updateInfo(Long id, BusinessUnitUpdateCommand command) {
-
+        Franchise franchise = franchiseRepository.findById(id)
+                .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
+        franchise.updateFranchiseInfo(command);
     }
 
+    // 가맹점 등록
     @Override
-    public void create(BusinessUnitInternal businessUnitInternal) {
-
+    public void create(BusinessUnitCreateCommand command) {
+        Franchise franchise = Franchise.from(command);
+        franchiseRepository.save(franchise);
     }
 
+    // 가맹점 목록 조회
+    @Override
+    public Page<BusinessUnitInternal> getBusinessUnitList(Pageable pageable) {
+        return franchiseRepository.findAll(pageable).map(BusinessUnitInternal::from);
+    }
+
+    // 가맹점 상태 변경
     @Override
     public void updateStatus(Long id, UsableStatus status) {
-
+        Franchise franchise = franchiseRepository.findById(id)
+                .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
+        franchise.updateStatus(status);
     }
 
+    // 가맹점 삭제
     @Override
     public void delete(Long id) {
-
+        Franchise franchise = franchiseRepository.findById(id)
+                .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
+        franchiseRepository.delete(franchise);
     }
 }

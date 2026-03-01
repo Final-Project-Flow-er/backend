c0131b0bea71dfdfeba4b8f58419b77b1b669404
rlatjddms <kseo_o@naver.com>
2026-03-02 00:26:36 +0900

    [feat] #78 BusinessUnitManagementService 메서드 추가

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/BusinessUnitManagementService.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/BusinessUnitManagementService.java
@@ -1,11 +1,15 @@
 package com.chaing.domain.businessunits.service;
 
 import com.chaing.core.enums.UsableStatus;
+import com.chaing.domain.businessunits.dto.command.BusinessUnitCreateCommand;
 import com.chaing.domain.businessunits.dto.internal.BusinessUnitInternal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BusinessUnitManagementService extends BusinessUnitService {
 
-    void create(BusinessUnitInternal businessUnitInternal);
+    void create(BusinessUnitCreateCommand command);
+    Page<BusinessUnitInternal> getBusinessUnitList(Pageable pageable);
     void updateStatus(Long id, UsableStatus status);
     void delete(Long id);
 }

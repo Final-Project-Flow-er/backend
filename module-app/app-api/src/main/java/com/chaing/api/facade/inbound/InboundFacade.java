517da93d4a2644375e77c8c6adb66f1f0427628c
yoocy0 <yoocy0328@gmail.com>
2026-03-01 10:16:36 +0900

    [fix] #72 Facade 내 @Validated 어노테이션 추가

--- module-app/app-api/src/main/java/com/chaing/api/facade/inbound/InboundFacade.java
+++ module-app/app-api/src/main/java/com/chaing/api/facade/inbound/InboundFacade.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @Getter
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Validated
 public class InboundFacade {
 
     public List<InboundBoxSummaryResponse> getPendingBoxes(@Valid InboundBoxSummaryRequest request) {

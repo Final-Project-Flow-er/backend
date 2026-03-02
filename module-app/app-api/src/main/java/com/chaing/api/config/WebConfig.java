b1d1bf0f97fb273728fe10ac09c8a4a305f593d5
rlatjddms <kseo_o@naver.com>
2026-03-02 23:33:21 +0900

    [refactor] #78 WebConfig 코드 형식 개선

--- module-app/app-api/src/main/java/com/chaing/api/config/WebConfig.java
+++ module-app/app-api/src/main/java/com/chaing/api/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.chaing.api.config;
 import com.chaing.domain.businessunits.enums.BusinessUnitType;
 import com.chaing.domain.businessunits.exception.BusinessUnitErrorCode;
 import com.chaing.domain.businessunits.exception.BusinessUnitException;
-import org.springframework.lang.Nullable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
@@ -16,15 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new Converter<String, BusinessUnitType>() {
-            @Override
-            public BusinessUnitType convert(@Nullable String source) {
-                if (source == null || source.isBlank()) return null;
-                try {
-                    return BusinessUnitType.valueOf(source.trim().toUpperCase(Locale.ROOT));
-                } catch (IllegalArgumentException e) {
-                    throw new BusinessUnitException(BusinessUnitErrorCode.INVALID_BUSINESS_UNIT_TYPE);
-                }
+        registry.addConverter((Converter<String, BusinessUnitType>) source -> {
+            if (source.isBlank()) return null;
+            try {
+                return BusinessUnitType.valueOf(source.trim().toUpperCase(Locale.ROOT));
+            } catch (IllegalArgumentException e) {
+                throw new BusinessUnitException(BusinessUnitErrorCode.INVALID_BUSINESS_UNIT_TYPE);
             }
         });
     }

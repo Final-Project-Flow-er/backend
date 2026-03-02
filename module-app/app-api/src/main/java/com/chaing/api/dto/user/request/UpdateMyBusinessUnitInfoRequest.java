d73a538243b947e8ee3c21770dc483effe6eda87
rlatjddms <kseo_o@naver.com>
2026-03-02 17:37:46 +0900

    [feat] #78 UpdateMyBusinessUnitInfoRequest 구현

---
+++ module-app/app-api/src/main/java/com/chaing/api/dto/user/request/UpdateMyBusinessUnitInfoRequest.java
@@ -0,0 +1,44 @@
+package com.chaing.api.dto.user.request;
+
+import com.chaing.api.dto.hq.management.request.BusinessUnitUpdateRequest;
+import com.chaing.api.dto.hq.management.request.BusinessUnitUpdateRequest.FranchiseUpdateRequest;
+import com.chaing.api.dto.hq.management.request.BusinessUnitUpdateRequest.FactoryUpdateRequest;
+
+import java.time.LocalTime;
+
+public record UpdateMyBusinessUnitInfoRequest(
+
+        String phone,
+
+        // 가맹점 전용
+        String operatingDays,
+        LocalTime openTime,
+        LocalTime closeTime,
+        String imageUrl,
+
+        // 공장 전용
+        Integer productionLineCount
+) {
+    public BusinessUnitUpdateRequest toManagementRequest() {
+        return new BusinessUnitUpdateRequest(
+                null,
+                null,
+                this.phone,
+                null,
+                null,
+                null,
+
+                new FranchiseUpdateRequest(
+                        this.operatingDays,
+                        this.openTime,
+                        this.closeTime,
+                        this.imageUrl,
+                        null,
+                        null,
+                        null
+                ),
+
+                this.productionLineCount != null ? new FactoryUpdateRequest(this.productionLineCount) : null
+        );
+    }
+}

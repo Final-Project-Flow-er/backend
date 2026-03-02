c1be8a44aae6a6c001b97a6a5e82688983917789
rlatjddms <kseo_o@naver.com>
2026-03-02 22:35:30 +0900

    [refactor] #78 사업장 코드 생성 시 동시성 문제 해결

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/repository/FranchiseRepository.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/repository/FranchiseRepository.java
@@ -1,7 +1,9 @@
 package com.chaing.domain.businessunits.repository;
 
 import com.chaing.domain.businessunits.entity.Franchise;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Franchise> findFirstByFranchiseCodeStartingWithOrderByFranchiseCodeDesc(String prefix);
 }

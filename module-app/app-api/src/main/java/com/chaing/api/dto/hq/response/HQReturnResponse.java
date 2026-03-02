f75d115ce02a5098fe0c23e51516ae465c847931
chaewookim <chaewookim2877@gmail.com>
2026-03-02 16:55:58 +0900

    [feat] #80 반품 조회 반환 DTO 구현

--- module-app/app-api/src/main/java/com/chaing/api/dto/hq/response/HQReturnResponse.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/hq/response/HQReturnResponse.java
@@ -1,7 +1,49 @@
 package com.chaing.api.dto.hq.response;
 
+import com.chaing.domain.returns.enums.ReturnStatus;
+import com.chaing.domain.returns.enums.ReturnType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Builder
-public record HQReturnResponse() {
+public record HQReturnResponse(
+        @NotBlank
+        String franchiseCode,
+
+        @NotNull
+        LocalDateTime requestedDate,
+
+        @NotBlank
+        String returnCode,
+
+        @NotNull
+        ReturnStatus status,
+
+        @NotBlank
+        String productCode,
+
+        @NotNull
+        ReturnType type,
+
+        @NotNull
+        @Min(1)
+        Integer quantity,
+
+        @NotNull
+        BigDecimal totalPrice,
+
+        @NotBlank
+        String reciever,
+
+        @NotBlank
+        String phoneNumber,
+
+        @NotBlank
+        String boxCode
+) {
 }

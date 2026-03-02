6838297b38e8df17cc540491b2bc97d654297e59
rlatjddms <kseo_o@naver.com>
2026-03-02 17:29:01 +0900

    [feat] #78 에러코드 추가

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/exception/BusinessUnitErrorCode.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/exception/BusinessUnitErrorCode.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 public enum BusinessUnitErrorCode implements ErrorCode {
 
     BUSINESS_UNIT_NOT_FOUND(404, "B001", "해당 사업장을 찾을 수 없습니다."),
+    INVALID_BUSINESS_UNIT_TYPE(400, "B002", "유효하지 않은 사업장 타입입니다."),
+    WARNING_ONLY_FOR_FRANCHISE(400, "B003", "가맹점을 위한 경고 로직입니다."),
     ;
 
     private final Integer status;

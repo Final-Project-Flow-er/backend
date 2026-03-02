6c857e40ae007e5fd190124f7f3ff83c70896af4
rlatjddms <kseo_o@naver.com>
2026-03-02 17:27:02 +0900

    [feat] #78 FranchiseServiceImpl 구현

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/impl/FranchiseServiceImpl.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/service/impl/FranchiseServiceImpl.java
@@ -1,6 +1,8 @@
 package com.chaing.domain.businessunits.service.impl;
 
 import com.chaing.core.enums.UsableStatus;
+import com.chaing.domain.businessunits.component.BusinessUnitCodeGenerator;
+import com.chaing.domain.businessunits.component.DistanceCalculator;
 import com.chaing.domain.businessunits.dto.command.BusinessUnitCreateCommand;
 import com.chaing.domain.businessunits.dto.command.BusinessUnitUpdateCommand;
 import com.chaing.domain.businessunits.dto.internal.BusinessUnitInternal;
@@ -19,6 +21,8 @@ import org.springframework.stereotype.Service;
 public class FranchiseServiceImpl implements BusinessUnitManagementService {
 
     private final FranchiseRepository franchiseRepository;
+    private final BusinessUnitCodeGenerator codeGenerator;
+    private final DistanceCalculator distanceCalculator;
 
     // 가맹점 아이디로 가맹점 조회
     @Override
@@ -30,17 +34,21 @@ public class FranchiseServiceImpl implements BusinessUnitManagementService {
 
     // 가맹점 정보 수정
     @Override
-    public void updateInfo(Long id, BusinessUnitUpdateCommand command) {
+    public BusinessUnitInternal updateInfo(Long id, BusinessUnitUpdateCommand command) {
         Franchise franchise = franchiseRepository.findById(id)
                 .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
         franchise.updateFranchiseInfo(command);
+        return BusinessUnitInternal.from(franchise);
     }
 
     // 가맹점 등록
     @Override
-    public void create(BusinessUnitCreateCommand command) {
-        Franchise franchise = Franchise.from(command);
+    public BusinessUnitInternal create(BusinessUnitCreateCommand command) {
+        String generatedCode = codeGenerator.generateFranchiseCode(command.region());
+        Double distance = distanceCalculator.calculate(command.address());
+        Franchise franchise = Franchise.from(command, generatedCode, distance);
         franchiseRepository.save(franchise);
+        return BusinessUnitInternal.from(franchise);
     }
 
     // 가맹점 목록 조회
@@ -51,10 +59,11 @@ public class FranchiseServiceImpl implements BusinessUnitManagementService {
 
     // 가맹점 상태 변경
     @Override
-    public void updateStatus(Long id, UsableStatus status) {
+    public BusinessUnitInternal updateStatus(Long id, UsableStatus status) {
         Franchise franchise = franchiseRepository.findById(id)
                 .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
         franchise.updateStatus(status);
+        return BusinessUnitInternal.from(franchise);
     }
 
     // 가맹점 삭제
@@ -63,5 +72,14 @@ public class FranchiseServiceImpl implements BusinessUnitManagementService {
         Franchise franchise = franchiseRepository.findById(id)
                 .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
         franchiseRepository.delete(franchise);
+    }
+
+    // 가맹점 경고 부여
+    public BusinessUnitInternal addWarning(Long id) {
+        Franchise franchise = franchiseRepository.findById(id)
+                .orElseThrow(() -> new BusinessUnitException(BusinessUnitErrorCode.BUSINESS_UNIT_NOT_FOUND));
+        franchise.addWarning();
+
+        return BusinessUnitInternal.from(franchise);
     }
 }

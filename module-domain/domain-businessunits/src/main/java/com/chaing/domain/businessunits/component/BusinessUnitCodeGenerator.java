1a41257f359d1f01c5a5c84595713c690858ab20
rlatjddms <kseo_o@naver.com>
2026-03-02 17:28:39 +0900

    [feat] #78 BusinessUnitCodeGenerator 컴포넌트 추가

---
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/component/BusinessUnitCodeGenerator.java
@@ -0,0 +1,39 @@
+package com.chaing.domain.businessunits.component;
+
+import com.chaing.core.enums.Region;
+import com.chaing.domain.businessunits.repository.FactoryRepository;
+import com.chaing.domain.businessunits.repository.FranchiseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BusinessUnitCodeGenerator {
+
+    private final FranchiseRepository franchiseRepository;
+    private final FactoryRepository factoryRepository;
+
+    // 가맹점 코드 생성
+    public String generateFranchiseCode(Region region) {
+        String prefix = region.getCode();
+        return franchiseRepository.findFirstByFranchiseCodeStartingWithOrderByFranchiseCodeDesc(prefix)
+                .map(franchise -> {
+                    String lastFullCode = franchise.getFranchiseCode();
+                    int nextNum = Integer.parseInt(lastFullCode.substring(prefix.length())) + 1;
+                    return String.format("%s%02d", prefix, nextNum);
+                })
+                .orElse(prefix + "01");
+    }
+
+    // 공장 코드 생성
+    public String generateFactoryCode() {
+        String prefix = "FA";
+        return factoryRepository.findFirstByFactoryCodeStartingWithOrderByFactoryCodeDesc(prefix)
+                .map(factory -> {
+                    String lastFullCode = factory.getFactoryCode();
+                    int nextNum = Integer.parseInt(lastFullCode.substring(prefix.length())) + 1;
+                    return String.format("%s%02d", prefix, nextNum);
+                })
+                .orElse(prefix + "01");
+    }
+}

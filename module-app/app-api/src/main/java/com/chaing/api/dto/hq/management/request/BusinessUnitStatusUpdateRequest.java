633fbc5e335c868feefb5bf94a4967b3070a6a02
rlatjddms <kseo_o@naver.com>
2026-03-02 17:32:05 +0900

    [feat] #78 request dto 구현

---
+++ module-app/app-api/src/main/java/com/chaing/api/dto/hq/management/request/BusinessUnitStatusUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.chaing.api.dto.hq.management.request;
+
+import com.chaing.core.enums.UsableStatus;
+import jakarta.validation.constraints.NotNull;
+
+public record BusinessUnitStatusUpdateRequest(
+
+        @NotNull(message = "변경할 상태값은 필수입니다.")
+        UsableStatus status
+) {
+}

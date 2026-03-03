74fd9e2bf821018f5d68c5e28a132ec48e412540
chaewookim <chaewookim2877@gmail.com>
2026-03-03 14:31:21 +0900

    [feat] #80 본사 특정 반품 조회에 사용하는 DTO 구현

--- module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/response/HQReturnDetailResponse.java
+++ module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/response/HQReturnDetailResponse.java
@@ -1,11 +1,14 @@
 package com.chaing.domain.returns.dto.response;
 
+import com.chaing.domain.returns.enums.ReturnStatus;
 import com.chaing.domain.returns.enums.ReturnType;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
 public record HQReturnDetailResponse(
         String returnCode,
 
@@ -20,6 +23,8 @@ public record HQReturnDetailResponse(
         String phoneNumber,
 
         ReturnType type,
+
+        ReturnStatus status,
 
         String description,
 
--- module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/response/HQReturnItemInfo.java
+++ module-domain/domain-returns/src/main/java/com/chaing/domain/returns/dto/response/HQReturnItemInfo.java
@@ -1,4 +1,8 @@
 package com.chaing.domain.returns.dto.response;
 
-public record HQReturnItemInfo() {
+public record HQReturnItemInfo(
+        Long returnItemId,
+
+        Long orderItemId
+) {
 }

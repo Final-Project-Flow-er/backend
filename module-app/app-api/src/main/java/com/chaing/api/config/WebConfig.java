5d262eaac2781c6ab0dbbaeb91bab4407ec7d3d9
rlatjddms <kseo_o@naver.com>
2026-03-03 11:31:35 +0900

    [fix] #78 WebConfig 에러 해결

--- module-app/app-api/src/main/java/com/chaing/api/config/WebConfig.java
+++ module-app/app-api/src/main/java/com/chaing/api/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.chaing.domain.businessunits.enums.BusinessUnitType;
 import com.chaing.domain.businessunits.exception.BusinessUnitErrorCode;
 import com.chaing.domain.businessunits.exception.BusinessUnitException;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,8 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter((Converter<String, BusinessUnitType>) source -> {
-            if (source.isBlank()) return null;
+        registry.addConverter(String.class, BusinessUnitType.class, source -> {
+            if (source == null || source.isBlank()) {
+                return null;
+            }
             try {
                 return BusinessUnitType.valueOf(source.trim().toUpperCase(Locale.ROOT));
             } catch (IllegalArgumentException e) {

b042feb78b669ad45c9ee540b7328320665d78fb
yk5095 <kyk5095@gmail.com>
2026-03-04 15:52:53 +0900

    [feat] #50 본사, 일별 가맹점 상세 탭 request 구현

--- module-app/app-api/src/main/java/com/chaing/api/dto/hq/settlement/request/HQSettlementFranchiseDailyDetailRequest.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/hq/settlement/request/HQSettlementFranchiseDailyDetailRequest.java
@@ -4,16 +4,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.YearMonth;
+import java.time.LocalDate;
 
 public record HQSettlementFranchiseDailyDetailRequest(
         @Schema(description = "가맹점 ID (PathVariable)", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull
         Long franchiseId,
 
-        @Schema(description = "조회 월(yyyy-MM)", example = "2026-02", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "조회 날짜", example = "2026-02-26", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull
-        @DateTimeFormat(pattern = "yyyy-MM")
-        YearMonth month
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        LocalDate date
 ) {
 }

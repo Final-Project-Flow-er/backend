7fd3aa20a5af383d4863b70762f4ca17c3e49082
rlatjddms <kseo_o@naver.com>
2026-03-04 09:32:31 +0900

    [feat] #84 Notice 엔티티 메서드 추가

--- module-domain/domain-notices/src/main/java/com/chaing/domain/notices/entity/Notice.java
+++ module-domain/domain-notices/src/main/java/com/chaing/domain/notices/entity/Notice.java
@@ -1,6 +1,8 @@
 package com.chaing.domain.notices.entity;
 
 import com.chaing.core.entity.BaseEntity;
+import com.chaing.domain.notices.dto.command.NoticeCreateCommand;
+import com.chaing.domain.notices.dto.command.NoticeUpdateCommand;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,12 +13,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at IS NULL")
 public class Notice extends BaseEntity {
 
     @Id
@@ -35,4 +39,20 @@ public class Notice extends BaseEntity {
     @Builder.Default
     @Column(nullable = false)
     private boolean important = false;
+
+    public static Notice createNotice(NoticeCreateCommand command, Long authorId) {
+        return Notice.builder()
+                .title(command.title())
+                .content(command.content())
+                .authorId(authorId)
+                .important(command.important())
+                .build();
+    }
+
+    public void updateNotice(NoticeUpdateCommand command, Long authorId) {
+        this.title = command.title();
+        this.content = command.content();
+        this.authorId = authorId;
+        this.important = command.important();
+    }
 }

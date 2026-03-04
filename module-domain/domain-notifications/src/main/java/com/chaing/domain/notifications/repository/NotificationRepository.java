34133c2de666c8f0127fd1abe8d18df56a835e47
rlatjddms <kseo_o@naver.com>
2026-03-04 17:40:28 +0900

    [feat] #88 NotificationRepository 추가

---
+++ module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/repository/NotificationRepository.java
@@ -0,0 +1,19 @@
+package com.chaing.domain.notifications.repository;
+
+import com.chaing.domain.notifications.entity.Notification;
+import com.chaing.domain.notifications.enums.NotificationType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Page<Notification> findAllByUserIdOrderByNotificationIdDesc(Long userId, Pageable pageable);
+    List<Notification> findAllByUserIdAndIsReadFalse(Long userId);
+    List<Notification> findAllByTypeAndTargetId(NotificationType type, Long targetId);
+    void deleteAllByTypeAndTargetId(NotificationType type, Long targetId);
+}

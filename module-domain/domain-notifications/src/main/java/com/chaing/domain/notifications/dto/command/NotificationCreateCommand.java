f068660cba17279385a88d7cb271943cd91928a0
rlatjddms <kseo_o@naver.com>
2026-03-04 20:10:38 +0900

    [fix] #88 Validation 타입 수정

--- module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/dto/command/NotificationCreateCommand.java
+++ module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/dto/command/NotificationCreateCommand.java
@@ -2,15 +2,16 @@ package com.chaing.domain.notifications.dto.command;
 
 import com.chaing.domain.notifications.enums.NotificationType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record NotificationCreateCommand(
 
-        @NotBlank(message = "알림 수신자는 필수입니다.")
+        @NotNull(message = "알림 수신자는 필수입니다.")
         Long userId,
 
-        @NotBlank(message = "알림 타입은 필수입니다.")
+        @NotNull(message = "알림 타입은 필수입니다.")
         NotificationType type,
 
         @NotBlank(message = "알림 메시지는 필수입니다.")

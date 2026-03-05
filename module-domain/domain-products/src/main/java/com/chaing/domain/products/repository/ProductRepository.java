87d5653638e256282f78d6e8e326326c70a71b51
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-06 00:24:29 +0900

    [feat] #82 상품 정보 메소드 구현

--- module-domain/domain-products/src/main/java/com/chaing/domain/products/repository/ProductRepository.java
+++ module-domain/domain-products/src/main/java/com/chaing/domain/products/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.chaing.domain.products.repository;
 import com.chaing.domain.products.entity.Product;
 import com.chaing.domain.products.repository.interfaces.ProductRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long>, ProductRepositoryCustom {
     List<Product> findAllByProductIdIn(List<Long> productIds);
+
+    @Query("SELECT p.productId FROM Product p")
+    List<Long> findAllProductIds();
 }

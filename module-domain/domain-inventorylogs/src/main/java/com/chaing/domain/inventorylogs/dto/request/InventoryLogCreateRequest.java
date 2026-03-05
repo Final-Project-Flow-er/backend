9a8897d5f655142eea3bb00e1dd6324ff65147d7
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-06 00:20:56 +0900

    [feat] #82 Request 구현

---
+++ module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/dto/request/InventoryLogCreateRequest.java
@@ -0,0 +1,24 @@
+package com.chaing.domain.inventorylogs.dto.request;
+
+import com.chaing.core.enums.LogType;
+import com.chaing.domain.inventorylogs.enums.ActorType;
+import com.chaing.domain.inventorylogs.enums.LocationType;
+
+import java.math.BigDecimal;
+
+public record InventoryLogCreateRequest(
+        Long productId,
+        String productName,
+        String boxCode,
+        String transactionCode,
+        LogType logType,
+        Integer quantity,
+        BigDecimal supplyPrice,
+        LocationType fromLocationType,
+        Long fromLocationId,
+        LocationType toLocationType,
+        Long toLocationId,
+        ActorType actorType,
+        Long actorId
+) {
+}

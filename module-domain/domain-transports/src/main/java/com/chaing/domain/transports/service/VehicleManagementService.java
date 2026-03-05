be5eeb9d8a4414c435f5fec65b41fd722ccbb9da
rlatjddms <kseo_o@naver.com>
2026-03-05 14:33:02 +0900

    [feat] #92 VehicleManagementService 구현

---
+++ module-domain/domain-transports/src/main/java/com/chaing/domain/transports/service/VehicleManagementService.java
@@ -0,0 +1,57 @@
+package com.chaing.domain.transports.service;
+
+import com.chaing.core.enums.UsableStatus;
+import com.chaing.domain.transports.dto.command.VehicleCreateCommand;
+import com.chaing.domain.transports.dto.command.VehicleUpdateCommand;
+import com.chaing.domain.transports.entity.Vehicle;
+import com.chaing.domain.transports.exception.TransportErrorCode;
+import com.chaing.domain.transports.exception.TransportException;
+import com.chaing.domain.transports.repository.VehicleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleManagementService {
+
+    private final VehicleRepository vehicleRepository;
+
+    // 운송 차량 등록
+    public Vehicle createVehicle(VehicleCreateCommand command) {
+        Vehicle vehicle = Vehicle.createVehicle(command);
+        return vehicleRepository.save(vehicle);
+    }
+
+    // 운송 차량 목록 조회
+    public Page<Vehicle> getVehicles(Pageable pageable) {
+        return vehicleRepository.findAll(pageable);
+    }
+
+    // 운송 차량 상세 조회
+    public Vehicle getById(Long id) {
+        return vehicleRepository.findById(id)
+                .orElseThrow(() -> new TransportException(TransportErrorCode.TRANSPORT_VEHICLE_NOT_FOUND));
+    }
+
+    // 운송 차량 수정
+    public Vehicle updateVehicle(Long id, VehicleUpdateCommand command) {
+        Vehicle vehicle = getById(id);
+        vehicle.updateVehicle(command);
+        return vehicle;
+    }
+
+    // 운송 차량 상태 변경
+    public Vehicle updateStatus(Long id, UsableStatus status) {
+        Vehicle vehicle = getById(id);
+        vehicle.updateStatus(status);
+        return vehicle;
+    }
+
+    // 운송 차량 삭제
+    public void deleteVehicle(Long id) {
+        Vehicle vehicle = getById(id);
+        vehicle.delete();
+    }
+}

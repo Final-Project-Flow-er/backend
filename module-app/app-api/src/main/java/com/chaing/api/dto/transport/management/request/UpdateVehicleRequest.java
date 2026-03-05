ad70c90a6e2973c24f53bca70a71b65e9cb931f9
rlatjddms <kseo_o@naver.com>
2026-03-05 21:05:28 +0900

    [feat] #92 운송 차량 request dto 구현

--- module-app/app-api/src/main/java/com/chaing/api/dto/transport/management/request/UpdateVehicleRequest.java
+++ module-app/app-api/src/main/java/com/chaing/api/dto/transport/management/request/UpdateVehicleRequest.java
@@ -1,4 +1,30 @@
 package com.chaing.api.dto.transport.management.request;
 
-public record UpdateVehicleRequest() {
+import com.chaing.core.enums.UsableStatus;
+import com.chaing.domain.transports.dto.command.VehicleUpdateCommand;
+import com.chaing.domain.transports.enums.Dispatchable;
+
+public record UpdateVehicleRequest(
+
+        Long transportId,
+        String vehicleNumber,
+        String vehicleType,
+        String driverName,
+        String driverPhone,
+        Long maxLoad,
+        Dispatchable dispatchable,
+        UsableStatus usableStatus
+) {
+    public VehicleUpdateCommand toCommand() {
+        return new VehicleUpdateCommand(
+                this.transportId,
+                this.vehicleNumber,
+                this.vehicleType,
+                this.driverName,
+                this.driverPhone,
+                this.maxLoad,
+                this.dispatchable,
+                this.usableStatus
+        );
+    }
 }

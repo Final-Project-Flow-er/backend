69674ba4b58e28d4f6d2e383b53195eeb9b5ed94
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-06 00:36:19 +0900

    [feat] #82 로그 기록 추가

--- module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/service/InventoryLogService.java
+++ module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/service/InventoryLogService.java
@@ -3,12 +3,17 @@ package com.chaing.domain.inventorylogs.service;
 
 import com.chaing.domain.inventorylogs.dto.request.FactoryLogRequest;
 import com.chaing.domain.inventorylogs.dto.request.FranchiseLogRequest;
+import com.chaing.domain.inventorylogs.dto.request.InventoryLogCreateRequest;
 import com.chaing.domain.inventorylogs.dto.request.LogRequest;
 import com.chaing.domain.inventorylogs.dto.response.FranchiseInventoryLogListResponse;
+import com.chaing.domain.inventorylogs.dto.response.FranchiseProductSalesResponse;
 import com.chaing.domain.inventorylogs.dto.response.InventoryLogListResponse;
+import com.chaing.domain.inventorylogs.entity.InventoryLog;
 import com.chaing.domain.inventorylogs.repository.InventoryLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +44,36 @@ public class InventoryLogService {
 
     public InventoryLogListResponse findFactoryInventoryLogs(Long factoryId, FactoryLogRequest request) {
         return inventoryLogRepository.findFactoryInventoryLogs(factoryId, request);
+    }
+
+    public List<FranchiseProductSalesResponse> getProductSales(List<Long> franchiseIds, List<Long> productIds) {
+        return inventoryLogRepository.getProductSales(franchiseIds, productIds);
+    }
+
+    public void recordInventoryLog(List<InventoryLogCreateRequest> logs) {
+        List<InventoryLog> entities = logs.stream()
+                .map(this::toEntity)
+                .toList();
+
+        inventoryLogRepository.saveAll(entities);
+    }
+
+    private InventoryLog toEntity(InventoryLogCreateRequest request) {
+        return InventoryLog.builder()
+                .productId(request.productId())
+                .productName(request.productName())
+                .boxCode(request.boxCode())
+                .transactionCode(request.transactionCode())
+                .logType(request.logType())             // 받을 때 스캔하면 INBOUND, 보낼때 스캔하면 OUTBOUND
+                .quantity(request.quantity())
+                .boxQuantity(1) // 현재는 항상 1
+                .supplyPrice(request.supplyPrice())
+                .fromLocationType(request.fromLocationType())
+                .fromLocationId(request.fromLocationId())
+                .toLocationType(request.toLocationType())
+                .toLocationId(request.toLocationId())
+                .actorType(request.actorType())
+                .actorId(request.actorId())
+                .build();
     }
 }

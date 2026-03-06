d598048e20f3dd627bff013fde94d8122ff2b8c6
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-06 16:25:44 +0900

    [refactor] #82 getProductSales 수정

--- module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/repository/interfaces/InventoryLogRepositoryCustom.java
+++ module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/repository/interfaces/InventoryLogRepositoryCustom.java
@@ -1,21 +1,28 @@
 package com.chaing.domain.inventorylogs.repository.interfaces;
 
+import com.chaing.core.enums.LogType;
 import com.chaing.domain.inventorylogs.dto.request.FactoryLogRequest;
 import com.chaing.domain.inventorylogs.dto.request.FranchiseLogRequest;
 import com.chaing.domain.inventorylogs.dto.request.LogRequest;
 import com.chaing.domain.inventorylogs.dto.response.FranchiseInventoryLogListResponse;
 import com.chaing.domain.inventorylogs.dto.response.FranchiseProductSalesResponse;
 import com.chaing.domain.inventorylogs.dto.response.InventoryLogListResponse;
+import com.chaing.domain.inventorylogs.enums.ActorType;
 
 import java.util.List;
 
 public interface InventoryLogRepositoryCustom {
     InventoryLogListResponse findReturnInboundLogs(LogRequest request);
+
     InventoryLogListResponse findReturnOutboundLogs(LogRequest request);
+
     InventoryLogListResponse findDisposalLogs(LogRequest request);
+
     FranchiseInventoryLogListResponse findFranchiseInboundOutboundLogs(Long franchiseId, FranchiseLogRequest request);
+
     FranchiseInventoryLogListResponse findFranchiseSalesRefundLogs(Long franchiseId, FranchiseLogRequest request);
+
     InventoryLogListResponse findFactoryInventoryLogs(Long factoryId, FactoryLogRequest request);
 
-    List<FranchiseProductSalesResponse> getProductSales(List<Long> franchiseId, List<Long> productIds);
+    List<FranchiseProductSalesResponse> getProductSales(List<Long> actorId, List<Long> productIds, ActorType actorType, LogType logType);
 }

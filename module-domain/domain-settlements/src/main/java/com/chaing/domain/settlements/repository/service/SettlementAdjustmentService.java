5580011a7c303859f4b3681dc6c7ee34ba0c32e2
yk5095 <kyk5095@gmail.com>
2026-03-06 10:02:25 +0900

    [feat] #96 전표 조정 페이지 구현

---
+++ module-domain/domain-settlements/src/main/java/com/chaing/domain/settlements/repository/service/SettlementAdjustmentService.java
@@ -0,0 +1,17 @@
+package com.chaing.domain.settlements.repository.service;
+
+import com.chaing.domain.settlements.entity.SettlementAdjustment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface SettlementAdjustmentService {
+
+    // 조정 전표 생성 (+ 누르고 작성)
+    SettlementAdjustment create(SettlementAdjustment adjustment);
+
+    // 목록 조회 (페이징)
+    Page<SettlementAdjustment> getAll(Pageable pageable);
+
+    // 단건 조회 (하단 상세)
+    SettlementAdjustment getById(Long id);
+}

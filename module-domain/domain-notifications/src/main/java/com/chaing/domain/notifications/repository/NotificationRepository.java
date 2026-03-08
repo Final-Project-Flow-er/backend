4afbe2d094614c3151db597fa04ac1102e0d7433
rlatjddms <kseo_o@naver.com>
2026-03-09 02:47:12 +0900

    [refactor] #106 알림 데이터 접근을 위한 쿼리 구현

--- module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/repository/NotificationRepository.java
+++ module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/repository/NotificationRepository.java
@@ -5,17 +5,27 @@ import com.chaing.domain.notifications.enums.NotificationType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    Page<Notification> findAllByUserIdOrderByUpdatedAtDesc(Long userId, Pageable pageable);
-    List<Notification> findAllByUserIdAndIsReadFalse(Long userId);
-    List<Notification> findAllByTypeAndTargetId(NotificationType type, Long targetId);
-    Optional<Notification> findByNotificationIdAndUserId(Long notificationId, Long userId);
+    Page<Notification> findAllByUserIdInOrderByUpdatedAtDesc(Collection<Long> userIds, Pageable pageable);
+    List<Notification> findAllByUserIdInAndIsReadFalse(Collection<Long> userIds);
     void deleteAllByTypeAndTargetId(NotificationType type, Long targetId);
+
+    @Query("SELECT n FROM Notification n WHERE n.notificationId = :notificationId AND (n.userId = :userId OR n.userId = 0)")
+    Optional<Notification> findByNotificationIdAndUserId(@Param("notificationId") Long notificationId, @Param("userId") Long userId);
+
+    @Modifying
+    @Query("DELETE FROM Notification n WHERE n.isRead = true AND n.createdAt < :targetDate")
+    void deleteOldNotifications(@Param("targetDate") LocalDateTime targetDate);
 }

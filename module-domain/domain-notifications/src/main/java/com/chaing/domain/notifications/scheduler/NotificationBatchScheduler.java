2803c13f18a460c73a9e6bd7c729ac4518baacb4
rlatjddms <kseo_o@naver.com>
2026-03-09 02:49:09 +0900

    [feat] #106 NotificationBatchScheduler 구현으로 6개월 지난 알림 제거

---
+++ module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/scheduler/NotificationBatchScheduler.java
@@ -0,0 +1,23 @@
+package com.chaing.domain.notifications.scheduler;
+
+import com.chaing.domain.notifications.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationBatchScheduler {
+
+    private final NotificationRepository notificationRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void deleteOldNotifications() {
+        LocalDateTime sixMonthsAgo = LocalDateTime.now().minusMonths(6);
+        notificationRepository.deleteOldNotifications(sixMonthsAgo);
+    }
+}

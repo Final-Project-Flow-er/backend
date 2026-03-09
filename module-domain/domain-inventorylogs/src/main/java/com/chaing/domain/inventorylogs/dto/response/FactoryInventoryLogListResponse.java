8930bbff4afb4c7c464c30f4caeb603f98196dda
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-09 17:50:19 +0900

    [feat] #112 Response추가

---
+++ module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/dto/response/FactoryInventoryLogListResponse.java
@@ -0,0 +1,12 @@
+package com.chaing.domain.inventorylogs.dto.response;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record FactoryInventoryLogListResponse(
+        List<FactoryInventoryLogResponse> factoryInventoryLogResponseList
+) {
+
+}

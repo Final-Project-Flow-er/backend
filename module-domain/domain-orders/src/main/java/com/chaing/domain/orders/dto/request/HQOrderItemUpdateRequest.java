ef9fdccf114ed3e86f15ba1c903d75424673e06f
chaewookim <chaewookim2877@gmail.com>
2026-03-09 17:04:59 +0900

    [refactor] #113 notBlank로 validation 강화

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/dto/request/HQOrderItemUpdateRequest.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/dto/request/HQOrderItemUpdateRequest.java
@@ -1,9 +1,10 @@
 package com.chaing.domain.orders.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record HQOrderItemUpdateRequest(
-        @NotNull
+        @NotBlank
         String productCode,
 
         @NotNull

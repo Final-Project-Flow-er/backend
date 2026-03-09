c2a32cded660e5a871a596a0c9041d5b41b98874
rlatjddms <kseo_o@naver.com>
2026-03-09 11:18:59 +0900

    [feat] #106 NotificationStatusRepository 추가

---
+++ module-domain/domain-notifications/src/main/java/com/chaing/domain/notifications/repository/NotificationStatusRepository.java
@@ -0,0 +1,14 @@
+package com.chaing.domain.notifications.repository;
+
+import com.chaing.domain.notifications.entity.NotificationStatus;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface NotificationStatusRepository extends CrudRepository<NotificationStatus, Long> {
+
+    Optional<NotificationStatus> findByUserIdAndNotificationId(Long userId, Long notificationId);
+    void deleteByUserIdAndNotificationId(Long userId, Long notificationId);
+}

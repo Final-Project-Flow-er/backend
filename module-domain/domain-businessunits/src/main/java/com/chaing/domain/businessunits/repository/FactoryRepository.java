02b397eed632a67958a1d6ca943387df529b0729
rlatjddms <kseo_o@naver.com>
2026-03-10 10:30:19 +0900

    [chore] #122 사용하지 않는 import 제거

--- module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/repository/FactoryRepository.java
+++ module-domain/domain-businessunits/src/main/java/com/chaing/domain/businessunits/repository/FactoryRepository.java
@@ -1,10 +1,8 @@
 package com.chaing.domain.businessunits.repository;
 
 import com.chaing.domain.businessunits.entity.Factory;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;

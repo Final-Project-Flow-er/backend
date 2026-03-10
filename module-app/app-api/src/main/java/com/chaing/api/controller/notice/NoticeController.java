74898a06d32d835b930e1a85ffaf0eb280effac2
rlatjddms <kseo_o@naver.com>
2026-03-11 02:52:35 +0900

    [refactor] #126 페이징 사이즈 설정

--- module-app/app-api/src/main/java/com/chaing/api/controller/notice/NoticeController.java
+++ module-app/app-api/src/main/java/com/chaing/api/controller/notice/NoticeController.java
@@ -13,6 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -46,7 +47,7 @@ public class NoticeController {
     @Operation(summary = "공지사항 목록 조회", description = "전체 공지사항 목록 조회")
     @GetMapping
     public ResponseEntity<ApiResponse<Page<NoticeSummaryResponse>>> getNotices(
-            Pageable pageable
+            @PageableDefault(size = 20) Pageable pageable
     ) {
         return ResponseEntity.ok(ApiResponse.success(noticeFacade.getNoticeList(pageable)));
     }

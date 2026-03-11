4be04a3f72ccaecce72acee5ec237323883a9c97
cho-yunho01 <04whdbsgh30@naver.com>
2026-03-11 16:34:03 +0900

    [refactor] #136 Object 변환

--- module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/repository/impl/InventoryLogRepositoryImpl.java
+++ module-domain/domain-inventorylogs/src/main/java/com/chaing/domain/inventorylogs/repository/impl/InventoryLogRepositoryImpl.java
@@ -39,6 +39,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -490,7 +491,7 @@ public class InventoryLogRepositoryImpl implements InventoryLogRepositoryCustom 
                                 actorIdsCondition = log.actorId.in(nonNullActorIds);
                         }
 
-                        if (actorIds.contains(null)) {
+                        if (actorIds.stream().anyMatch(Objects::isNull)) {
                                 actorIdsCondition = (actorIdsCondition == null)
                                                 ? log.actorId.isNull()
                                                 : actorIdsCondition.or(log.actorId.isNull());

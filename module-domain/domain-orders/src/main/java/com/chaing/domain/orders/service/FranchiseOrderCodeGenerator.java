cc4dd1526fcbf5fbae41cabc179182f7ae64f920
chaewookim <chaewookim2877@gmail.com>
2026-03-16 17:24:25 +0900

    [refactor] #185 번호 생성기에 Redis 적용

--- module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderCodeGenerator.java
+++ module-domain/domain-orders/src/main/java/com/chaing/domain/orders/service/FranchiseOrderCodeGenerator.java
@@ -1,15 +1,23 @@
 package com.chaing.domain.orders.service;
 
 import com.chaing.core.util.CodeGenerator;
+import com.chaing.core.util.RedisSequenceGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@RequiredArgsConstructor
 public class FranchiseOrderCodeGenerator implements CodeGenerator {
+
+    private final RedisSequenceGenerator redisSequenceGenerator;
+
     @Override
     public String generate(String franchiseCode) {
-        return franchiseCode + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")) + (int)(Math.random()*100 + 1);
+        String today = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+        String seq = redisSequenceGenerator.nextSequence("franchise-order");
+        return franchiseCode + today + seq;
     }
 }

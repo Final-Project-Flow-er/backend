2f804161d9fe3e9d29204cfbf11ab795e55845a4
yoocy0 <yoocy0328@gmail.com>
2026-03-19 02:41:25 +0900

    [fix] #218 오ë류 해결

--- module-app/app-api/src/main/java/com/chaing/api/facade/inbound/InboundFacade.java
+++ module-app/app-api/src/main/java/com/chaing/api/facade/inbound/InboundFacade.java
@@ -18,10 +18,10 @@ import com.chaing.domain.inventories.exception.InventoriesErrorCode;
 import com.chaing.domain.inventories.exception.InventoriesException;
 import com.chaing.domain.inventories.service.InventoryService;
 import com.chaing.domain.inventories.service.inbound.InboundService;
-import com.chaing.domain.inventorylogs.service.InventoryLogService;
 import com.chaing.domain.orders.dto.response.FranchiseOrderForTransitResponse;
 import com.chaing.domain.orders.service.FranchiseOrderService;
 import com.chaing.domain.products.service.ProductService;
+import com.chaing.domain.transports.service.InternalTransportService;
 import com.chaing.domain.users.enums.UserRole;
 import jakarta.validation.Valid;
 import lombok.Getter;
@@ -53,6 +53,7 @@ public class InboundFacade {
     private final FranchiseOrderService orderService;
     private final InventoryService inventoryService;
     private final InventoryLogFacade inventoryLogFacade;
+    private final InternalTransportService transportService;
 
     @Retryable(retryFor = ObjectOptimisticLockingFailureException.class, maxAttempts = 3, backoff = @Backoff(delay = 100, multiplier = 2))
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
